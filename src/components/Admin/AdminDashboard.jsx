6ce370ae3097ab769e66d32405e517ec231aba11
baudoin <baudoinvicbolingo@gmail.com>
2025-02-20 17:04:26 +0200

    debuging cordinator page

--- src/components/Admin/AdminDashboard.jsx
+++ src/components/Admin/AdminDashboard.jsx
@@ -119,13 +119,17 @@ function AdminDashboard() {
               <ChevronRight size={16} className="text-gray-400" />
             </Link>
           ))}
-          <button
-            onClick={handleLogout}
-            className="flex items-center w-full px-6 py-3 mt-4 text-gray-700 hover:bg-red-50 hover:text-red-600 transition-colors duration-200"
-          >
-            <LogOut size={20} />
-            <span className="ml-3">Logout</span>
-          </button>
+      
+
+          <div className="pt-64">
+            <button
+              onClick={handleLogout}
+              className="flex items-center w-full px-6 py-3 mt-4 text-gray-700 hover:bg-red-50 hover:text-red-600 transition-colors duration-200"
+            >
+              <LogOut size={20} />
+              <span className="ml-3">Logout</span>
+            </button>
+          </div>
         </nav>
       </div>
 
--- src/components/Coordinator/opportunities.jsx
+++ src/components/Coordinator/opportunities.jsx
@@ -94,13 +94,7 @@ function InternshipOpportunitiesPage() {
               <ChevronRight size={16} className="text-gray-400" />
             </Link>
           ))}
-          <button
-            onClick={handleLogout}
-            className="flex items-center w-full px-6 py-3 mt-4 text-gray-700 hover:bg-red-50 hover:text-red-600 transition-colors duration-200"
-          >
-            <LogOut size={20} />
-            <span className="ml-3">Logout</span>
-          </button>
+     
         </nav>
       </div>
 
