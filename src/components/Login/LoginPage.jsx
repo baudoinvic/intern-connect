b00eaaea06705cd5ee13ac5fb606180d835d863b
baudoin <baudoinvicbolingo@gmail.com>
2025-02-24 13:47:00 +0200

    about to fetch

--- src/components/Login/LoginPage.jsx
+++ src/components/Login/LoginPage.jsx
@@ -77,12 +77,12 @@ function LoginPage() {
                   <User className="h-5 w-5 text-gray-400" />
                 </div>
                 <input
-                  id="username"
+                  id="email"
                   type="text"
-                  value={username}
-                  onChange={(e) => setUsername(e.target.value)}
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
                   className="block w-full pl-10 pr-3 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-                  placeholder="Enter your username"
+                  placeholder="Enter your email"
                 />
               </div>
             </div>
